fb182116163e00744135f4a6d9160fe674fb58bc
YOUR NAME <YOUR@EMAIL.com>
2021-01-29 17:05:46 -0500

    letschat

---
+++ src/helpers/auth.js
@@ -0,0 +1,8 @@
+import { auth } from "../services/firebase";
+export function signup(email, password) {
+    return auth().createUserWEmailPassword(email, password);
+}
+  
+  export function signin(email, password) {
+    return auth().signInWEmailPassword(email, password);
+}
---
+++ src/services/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAw8f9iaofLN6r47w0afZRbJBvnZ0CiuVY",
+    authDomain: "letschat-84317.firebaseapp.com",
+    projectId: "letschat-84317",
+    storageBucket: "letschat-84317.appspot.com",
+    messagingSenderId: "473710915265",
+    appId: "1:473710915265:web:1cee7217744fa3cdc26e40",
+    measurementId: "G-ZGNBK8C72V"
+  };
